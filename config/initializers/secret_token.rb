dcb5dd643234df8dbae0be792227e91dc016048b
Keith Ryu <kibaekr@gmail.com>
2013-12-18 13:56:55 +0900

    First commit. Set up T-Bootstrap;

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PollingApp::Application.config.secret_key_base = '2ccaafcfba7a298f0cfb4b08fc09cea383538b27ead80f23aa42ae9518ca12d9986a65b2553e0898297325108eeaddc1c44eaf9da8d64ee8a5370e1a1decbcaf'
